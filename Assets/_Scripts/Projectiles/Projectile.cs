3f6d3c5958a6f5ab2dcdf9043912d6073faa77ea
santiago gutierrez <santigutih2704@gmail.com>
2022-10-11 18:19:10 -0300

    Collision events y remover pivote

--- Assets/_Scripts/Projectiles/Projectile.cs
+++ Assets/_Scripts/Projectiles/Projectile.cs
@@ -1,15 +1,20 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Game.Utils;
 
 namespace Game.Projectiles
 {
+    [RequireComponent(typeof(Collider2D))]
     public class Projectile : MonoBehaviour
     {
         public static List<Projectile> projectiles = new List<Projectile>();
         [SerializeField] protected Rigidbody2D rb;
         [SerializeField] protected bool _moveable = true;
         public bool Moveable { get => _moveable; }
+        [SerializeField] protected bool _destructible = true;
+        public bool Destructible { get => _destructible; }
+        [SerializeField] GameObject _container;
         public object owner;
         [SerializeField] ProjectileStats _stats;
         public ProjectileStats Stats { get => _stats; protected set => _stats = value; }
@@ -53,7 +58,6 @@ namespace Game.Projectiles
         }
 
         #endregion
-
         private void OnEnable()
         {
             projectiles.Add(this);
@@ -66,6 +70,8 @@ namespace Game.Projectiles
         {
             projectiles.Remove(this);
             onDestroy?.Invoke();
+            if (_container) Destroy(_container);
+            else Destroy(gameObject);
         }
         private void Update()
         {
@@ -81,14 +87,74 @@ namespace Game.Projectiles
         }
         protected virtual void AIUpdate() { }
         protected virtual void AIFixedUpdate() { }
+        public virtual void Despawn()
+        {
+            onDespawn?.Invoke();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            int collisionLayer = collision.gameObject.layer;
+            if (collisionLayer == Layers.GROUND) OnEnterGround(collision);
+            else if (collisionLayer == Layers.PLAYER) OnEnterPlayer(collision);
+            else if (collisionLayer == Layers.NPC) OnEnterNpc(collision);
+        }
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            int collisionLayer = collision.gameObject.layer;
+            if (collisionLayer == Layers.GROUND) OnStayGround(collision);
+            else if (collisionLayer == Layers.PLAYER) OnStayPlayer(collision);
+            else if (collisionLayer == Layers.NPC) OnStayNpc(collision);
+        }
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            int collisionLayer = collision.gameObject.layer;
+            if (collisionLayer == Layers.GROUND) OnExitGround(collision);
+            else if (collisionLayer == Layers.PLAYER) OnExitPlayer(collision);
+            else if (collisionLayer == Layers.NPC) OnExitNpc(collision);
+        }
 
         #region Events
         public event Action onDestroy;
-        public event Action onHit;
-        protected void OnHit() => onHit?.Invoke();
+        public event Action<Collider2D> onEnterNpc;
+        protected void OnEnterNpc(Collider2D collision) => onEnterNpc?.Invoke(collision);
+        public event Action<Collider2D> onEnterGround;
+        protected void OnEnterGround(Collider2D collision) => onEnterGround?.Invoke(collision);
+        public event Action<Collider2D> onEnterPlayer;
+        protected void OnEnterPlayer(Collider2D collision) => onEnterPlayer?.Invoke(collision);
+        public event Action<Collider2D> onStayNpc;
+        protected void OnStayNpc(Collider2D collision) => onStayNpc?.Invoke(collision);
+        public event Action<Collider2D> onStayGround;
+        protected void OnStayGround(Collider2D collision) => onStayGround?.Invoke(collision);
+        public event Action<Collider2D> onStayPlayer;
+        protected void OnStayPlayer(Collider2D collision) => onStayPlayer?.Invoke(collision);
+        public event Action<Collider2D> onExitNpc;
+        protected void OnExitNpc(Collider2D collision) => onExitNpc?.Invoke(collision);
+        public event Action<Collider2D> onExitGround;
+        protected void OnExitGround(Collider2D collision) => onExitGround?.Invoke(collision);
+        public event Action<Collider2D> onExitPlayer;
+        protected void OnExitPlayer(Collider2D collision) => onExitPlayer?.Invoke(collision);
+
         public event Action onLifeTimeEnd;
         protected void OnLifeTimeEnd() => onLifeTimeEnd?.Invoke();
+        public event Action onDespawn;
+        protected void OnDespawn() => onDespawn?.Invoke();
         #endregion
+        public static void Clear()
+        {
+            for (int i = projectiles.Count - 1; i >= 0; i--)
+            {
+                projectiles[i].Despawn();
+            }
+            for (int i = projectiles.Count - 1; i >= 0; i--)
+            {
+                projectiles[i].Despawn();
+            }
+            for (int i = projectiles.Count - 1; i >= 0; i--)
+            {
+                projectiles[i].Despawn();
+            }
+        }
     }
 }
 
--- Assets/_Scripts/Projectiles/SwingProjectile.cs
+++ Assets/_Scripts/Projectiles/SwingProjectile.cs
@@ -4,7 +4,6 @@ namespace Game.Projectiles
 {
     public class SwingProjectile : Projectile
     {
-        [SerializeField] bool hasPivot = true;
         Vector2 _;
         new public Vector2 Velocity { get => Vector2.zero; set => _ = value; }
         protected override void Initialize(object owner, ProjectileDataSO data, float damage, ProjectileState state, Vector2 velocity)
@@ -12,13 +11,6 @@ namespace Game.Projectiles
             this.owner = owner;
             Stats = data.Stats.CreateStats(damage, state);
             Stats.lifeTime = float.MaxValue;
-        }
-
-        new public void LifeTimeEnd()
-        {
-            OnLifeTimeEnd();
-            if (hasPivot) Destroy(transform.parent);
-            else Destroy(gameObject);
         }
     }
 }
